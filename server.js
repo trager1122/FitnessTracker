b8c1d98471a5ee47a40ccf2ae773ab97913e442f
Thomas Rager <tomrager@yahoo.com>
2021-01-15 12:39:41 -0500

    working on getting last workout route correct

--- server.js
+++ server.js
@@ -16,31 +16,31 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", { useNewUrlParser: true });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "./public/index.html"));
+// API Routes
+app.get("/api/workouts", (req, res) => {
+  Workout.findOne().sort({ field: 'asc', day: -1 }).limit(1), (error,last)=> {
+      if (error) {
+        console.log(error);
+      } else {
+        res.json(last);
+      }
+  }
 });
 
-app.get("/exercise/api/workouts", ({ body }, res) => {
+app.post("/api/workouts", ({ body }, res) => {
   const ex = body;
 
-  db.workouts.find(ex, (error, last) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(last);
-      }
-  });
-});;
-
-app.post("/exercise/api/workouts", ({ body }, res) => {
-  const ex = body;
-
-  db.workouts.insert(ex, (error, created) => {
+  Workout.insert(ex, (error, created) => {
     if (error) {
       res.send(error);
     } else {
       res.send(created);
     }
+});
+
+  // HTML Routes
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
   });
 });
 
